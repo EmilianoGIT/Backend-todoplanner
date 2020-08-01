d37800d61b52c56568301b49a1daed040e137137
EmilianoGIT <emiliano.barigelli@gmail.com>
2020-08-01 17:47:39 +0200

    added google recaptcha check for /register api

--- src/main/java/com/todo/rest/webservices/restfulwebservices/jwt/controller/JwtAuthenticationController.java
+++ src/main/java/com/todo/rest/webservices/restfulwebservices/jwt/controller/JwtAuthenticationController.java
@@ -1,26 +1,34 @@
 package com.todo.rest.webservices.restfulwebservices.jwt.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.todo.rest.webservices.restfulwebservices.exceptionhandler.ErrorResponse;
+import com.todo.rest.webservices.restfulwebservices.exceptionhandler.JsonExceptionHandler;
 import com.todo.rest.webservices.restfulwebservices.jwt.model.JwtRequest;
 import com.todo.rest.webservices.restfulwebservices.jwt.model.JwtResponse;
 import com.todo.rest.webservices.restfulwebservices.jwt.service.JwtUserDetailsService;
 import com.todo.rest.webservices.restfulwebservices.jwt.config.JwtTokenUtil;
 import com.todo.rest.webservices.restfulwebservices.user.UserDTO;
 import com.todo.rest.webservices.restfulwebservices.utilities.ConfigUtility;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.header.Header;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-
+import java.lang.module.Configuration;
 
 
 @RestController
@@ -37,10 +45,18 @@ public class JwtAuthenticationController {
     @Autowired
     private JwtUserDetailsService userDetailsService;
 
+    @Autowired
+    private ConfigUtility configUtil;
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO user, @RequestHeader("Recaptcha-Response") String recaptchaResponse) throws Exception {
+
+        if(clientIsNotARobot(recaptchaResponse))
         return ResponseEntity.ok(userDetailsService.save(user));
+        else{
+            throw new Exception("Recaptcha not valid.");
+            //return (ResponseEntity<?>) ResponseEntity.badRequest();
+        }
     }
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
@@ -65,5 +81,21 @@ public class JwtAuthenticationController {
             //throw new Exception("INVALID_CREDENTIALS", e);
             throw new Exception("Wrong username or password");
         }
+    }
+
+    private boolean clientIsNotARobot(String recaptchaResponse) throws Exception{
+
+        String recaptchaSharedSecretKey = configUtil.getProperty("recaptcha_shared_secret_key");
+
+        final String uri = "https://www.google.com/recaptcha/api/siteverify?"+
+                "secret=" + recaptchaSharedSecretKey
+                +"&response=" + recaptchaResponse;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String ApiResponse = restTemplate.postForObject( uri, null, String.class);
+
+        JsonObject jsonObject = new JsonParser().parse(ApiResponse).getAsJsonObject();
+        return  jsonObject.get("success").getAsBoolean();
+
     }
 }
